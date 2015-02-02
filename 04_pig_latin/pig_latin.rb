af4ab0b9d003c3f6c27b3306a7670ba3047c87b9
zhangmyihua <michaelzhang2@msn.com>
2015-02-02 16:17:18 -0500

    used a new until method to run pig latin, needs more refactoring to include phenomenon like qu in square

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,7 +1,13 @@
 def translate(word)
-	if word[0] == "a" || word[0] == "e" || word[0] == "i" || word[0] == "o"|| word[0] == "u"
-		word.to_s + "ay"
-	else
-		"#{word.to_s[1..-1]}#{word.to_s[0]}ay"
+	vowels = ["a", "e", "i", "o", "u"]
+	letters = word.chars
+
+	#a until state is a negative while statement.
+
+	until vowels.include?(letters[0])
+		letters.rotate!
 	end
+
+	"#{letters.join}ay"
 end
+
